4bbc2977f094065b2f378bf7fb50f95c05a8cf8e
73spica <15689977+73spica@users.noreply.github.com>
2016-12-20 14:08:50 +0900

    modify cornelius1 solver.

--- hacklu_CTF/2016/crypto/cornelius1/solver.py
+++ hacklu_CTF/2016/crypto/cornelius1/solver.py
@@ -1,16 +1,19 @@
 import requests
-
 import binascii
 import sys
 from base64 import b64decode
 import string
+from m1z0r3.crypro import *
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
 
 def brute(key):
     tmplen = 0
     for c in alpha:
         senddata = (key + c) * 10
         url = 'https://cthulhu.fluxfingers.net:1505/?user=%s'%senddata
-        r = requests.get(url)
+        r = requests.get(url,verify=False)
         deflate =  b64decode(r.cookies['auth'])
         l = len(deflate)
         # print "[+] senddata = " + senddata
@@ -20,7 +23,7 @@ def brute(key):
         tmplen = l
     return key+alpha[0]
 
-alpha = string.printable
+alpha = readable
 
 def main():
     nowans = 'flag:'
